2196ff09477356881d843c7703de910112b6e428
Roua <elhani.roua2017@gmail.com>
2026-03-09 11:57:51 +0100

    fix projet

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a7198b9e6060d94d7ac8c2ac50b8aac5ea7e46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69cfbea20f315842dd85ca8e7851123efbb25c17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
