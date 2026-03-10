bce6b32ca7196a4a37eb2f36e3d966ed5ca7ba56
Roua <roua.elheni@addinn.com>
2026-03-10 13:31:06 +0100

    fix code

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8c974482bfb41d40b3ab890b538916f86043c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debefeaa8e695f1980de700d2e366f94398e481a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistance/obj/Debug/net8.0/Persistance.AssemblyInfo.cs
+++ Persistance/obj/Debug/net8.0/Persistance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94316025876b68b0a794364dc2fee4f6e6559de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debefeaa8e695f1980de700d2e366f94398e481a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
