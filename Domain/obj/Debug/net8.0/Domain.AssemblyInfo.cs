bed4ab925c47abe49ea001ce4cf71d8b87d6473f
Meriem Jendoubi <meriemjandoubi2@gmail.com>
2026-03-13 05:22:07 +0100

    fix code quote et invoice

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8788dd7ca10a108b6187095113ef740aa0ce63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db35f35cb5d57955673841a76ad8f4c61b6d7441")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
