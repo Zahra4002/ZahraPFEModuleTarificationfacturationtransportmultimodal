510a50cb8da01dd3936a7758a03d5aee797cc4c7
Roua <roua.elheni@addinn.com>
2026-03-19 09:51:35 +0100

    fix code

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2935979df79e51ccaa26ff45f57baadabd843bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d921be30d2fd5d9b42cba5197fa75a3460dccb25")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
